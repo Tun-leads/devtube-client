f623bf330b77e38839638bc23c4f88cf7db7dccd
Aliyu Yakubu <57975594+Tun-leads@users.noreply.github.com>
2024-05-09 01:51:51 +0000

    Add files via upload

---
+++ devtube-client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Courses from './Courses';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Courses</h2>
+        <Courses />
+      </header>
+    </div>
+  );
+}
+
+export default App;
